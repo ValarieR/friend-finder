91879966a216c2dc340f08d809c6021a6f59f5a1
unknown <Valarie.Regas@gmail.com>
2017-11-10 21:46:36 -0500

    debugging again

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,7 +6,7 @@ var friends = [
       scores: [4,5,5,1,5,4,1,4,5,4]
     }, {
       name: "Michael",
-        img: "",
+        img: "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/983993_10101714726551508_397405967487025876_n.jpg?oh=2e15e5f47c72ec3363702c7e375494f9&oe=5AA4D9D4",
         scores: [2,5,4,5,1,2,3,1,1,3]
     }
   ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,9 +6,36 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
+
+        var bestMatch =  {
+            name: "",
+            img: "",
+            friendDiff: 1000
+        };
+
         var newFriend = req.body;
+        var userScores = newFriend.scores;
+        var totalDiff = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDiff = 0;
+
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                totalDiff += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDiff <= bestMatch.friendDiff) {
+
+                    bestMatch.name = friends[i].name;
+                    bestMatch.img = friends[i].img;
+                    bestMatch.friendDiff = totalDiff;
+                }
+            }
+        }
+
         friends.push(newFriend);
-        res.json(newFriend)
+        res.json(bestMatch);
     });
 };
 
