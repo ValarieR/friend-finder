0b6cd2e97c4e6fe1f454a6f8a59fa73c26ce412a
unknown <Valarie.Regas@gmail.com>
2017-11-10 22:29:34 -0500

    making minor tweaks in styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,27 @@
 var friends = [
     {
       name: "Valarie",
-      img: "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/23032724_10102031136059318_3263853372353110917_n.jpg?oh=e197b493f799a528e6f15bed49892762&oe=5A64DFF6",
+      img: "https://scontent.fatl1-1.fna.fbcdn.net/v/t31.0-8/14711039_10101469800819638_6247883944123664335_o.jpg?oh=f159d0495fba211fcce4c0c1a4ae93ae&oe=5A9D367C",
       scores: [4,5,5,1,5,4,1,4,5,4]
     }, {
       name: "Michael",
         img: "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/983993_10101714726551508_397405967487025876_n.jpg?oh=2e15e5f47c72ec3363702c7e375494f9&oe=5AA4D9D4",
         scores: [2,5,4,5,1,2,3,1,1,3]
+    }, {
+        name: "Anya",
+        photo: "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/22730316_10102020149037398_7977631425801111267_n.jpg?oh=2224b73f86a2f68979a9d603ab0586d3&oe=5A95EB94",
+        scores: [
+            4,
+            4,
+            5,
+            3,
+            5,
+            1,
+            5,
+            4,
+            4,
+            3
+        ]
     }
   ];
 
